08dfdd58c99fd3933c80888ce11cb1b5a6c27247
oralei <73796709+oralei@users.noreply.github.com>
2025-03-29 12:39:26 -0600

    hide and seek structs

--- CTCH312Project/Assets/Scripts/SeekManager.cs
+++ CTCH312Project/Assets/Scripts/SeekManager.cs
@@ -1,12 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SeekManager : MonoBehaviour
 {
     public interactableObject interactableObject;
+    public GameObject NPCBilly;
+
+    [System.Serializable]
+    public struct HidingSpot
+    {
+        public Vector3 Location;
+        public Quaternion Rotation;
+        public Animation Pose;
+    }
+
+    [SerializeField]
+    public List<HidingSpot> hidingSpots = new List<HidingSpot>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        foreach (var spot in hidingSpots)
+        {
+            Debug.Log($"Location: {spot.Location}, Rotation: {spot.Rotation}, Pose: {spot.Pose}");
+        }
     }
 
     // Update is called once per frame
